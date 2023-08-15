9bedc9470f6129d0137986721b4dcd3b5921302f
yangjf2019 <highfei2011@outlook.com>
2023-08-15 20:30:28 +0800

    新增 mvn 命令

---
+++ first-flink-demo/src/main/resources/mvn.sh
@@ -0,0 +1,8 @@
+mvn -B archetype:generate  \
+-DarchetypeGroupId=com.yjf \
+-DarchetypeArtifactId=maven-archetype-quickstart \
+-DarchetypeVersion=LATEST \
+-DgroupId=com.example.flinkonk8s \
+-DartifactId=first-flink-demo \
+-Dversion=1.0-SNAPSHOT \
+-DinteractiveMode=false
